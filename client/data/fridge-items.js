76bdbcdffd0580e6d61a23ef72df70c7124e2107
EDA <eda@mac-mini-5.local>
2017-06-02 09:58:46 +1200

    Initial data file generated

---
+++ client/data/fridge-items.js
@@ -0,0 +1,32 @@
+export default function () {
+  return [
+    {
+      id: 1,
+      item: 'cheese',
+      expDate: '10/10/2013',
+      owner: 'Tony',
+      shelf: 1
+    },
+    {
+      id: 2,
+      item: 'pizza',
+      expDate: '12/06/2017',
+      owner: 'Don',
+      shelf: 1
+    },
+    {
+      id: 3,
+      item: 'milk',
+      expDate: '02/06/2017',
+      owner: 'Rob',
+      shelf: 2
+    },
+    {
+      id: 4,
+      item: 'Roast Chicken',
+      expDate: '09/06/2017',
+      owner: 'Rory',
+      shelf: 3
+    }
+  ]
+}
