df532187f82f5e75627ff9e1adcd98dbface9910
EDA <eda@mac-mini-5.local>
2017-06-02 10:41:08 +1200

    Created inside fridge class/Container

--- client/containers/InsideFridge.jsx
+++ client/containers/InsideFridge.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
-const InsideFridge=( props => {
-  // console.log(props.fridge)
-  return (
-    <div>
+class InsideFridge extends Component {
+
+  createFridgeList () {
+    return this.props.fridge.map((food) => {
+      return (
+        <li key={food.id}>{food.item} {food.owner} {food.expDate} {food.shelf}</li>
+          // <div key={food.id}>
+          //   <li>{food.item}</li>
+          //   <li>{food.expDate}</li>
+          //   <li>{food.owner}</li>
+          //   <li>{food.shelf}</li>
+          //   <hr/>
+          // </div>
+      )
+    })
+  }
+  render () {
+    return (
       <ul>
-        {props.fridge.map( food => {
-          return (
-            <div key = {food.id}>
-            <li>{food.item}</li>
-            <li>{food.expDate}</li>
-            <li>{food.owner}</li>
-            <li>{food.shelf}</li>
-            <hr/>
-            </div>
-          )
-        } )}
+        {this.createFridgeList()}
       </ul>
-    </div>
-  )
-})
+    )
+  }
 
-
-
+}
 const mapStateToProps = (state) => {
   return {
     fridge: state.fridge
