5a6ece295162258f309e4b47b69baa69d1c687fe
EDA <eda@mac-mini-5.local>
2017-06-02 14:49:18 +1200

    Added deleteitem to all shelves

--- client/containers/ShelfTwo.jsx
+++ client/containers/ShelfTwo.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {deleteItem} from '../actions/index'
 
 class ShelfTwo extends Component {
 
@@ -7,7 +9,7 @@ class ShelfTwo extends Component {
     return this.props.fridge.map((food) => {
       if (Number(food.shelf) === 2) {
         return (
-          <div className='foodItem' key={food.id}>
+          <div className='foodItem' key={food.id} onClick={() => this.props.deleteItem(food)}>
             <p className='itemTitle'>{food.item}</p>
             <p className='itemOwner'>{food.owner}</p>
             <p className='itemExp'>{food.expDate}</p>
@@ -31,4 +33,8 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(ShelfTwo)
+function matchDispatchToProps (dispatch) {
+  return bindActionCreators({deleteItem: deleteItem}, dispatch)
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(ShelfTwo)
