71c78022e41cb6f0ec0f51ba3bc041298e70d543
EDA <eda@mac-mini-5.local>
2017-06-02 12:16:40 +1200

    Seperated out fridge shelves as containers

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import InsideFridge from '../containers/InsideFridge'
+import ShelfOne from '../containers/ShelfOne'
+import ShelfTwo from '../containers/ShelfTwo'
+import ShelfThree from '../containers/ShelfThree'
 
 
 const App = () => (
   <div className='app-container'>
-<InsideFridge />
+    <div className='fridge'>
+      <ShelfOne />
+      <ShelfTwo />
+      <ShelfThree />
+    </div>
   </div>
 )
 
--- client/containers/InsideFridge.jsx
+++ client/containers/InsideFridge.jsx
@@ -4,18 +4,54 @@ import {connect} from 'react-redux'
 class InsideFridge extends Component {
 
   createFridgeList () {
-    return this.props.fridge.map((food) => {
-      return (
-        <li key={food.id}>{food.item} {food.owner} {food.expDate} {food.shelf}</li>
-          // <div key={food.id}>
-          //   <li>{food.item}</li>
-          //   <li>{food.expDate}</li>
-          //   <li>{food.owner}</li>
-          //   <li>{food.shelf}</li>
-          //   <hr/>
-          // </div>
-      )
+
+    let shelfOneArr = []
+    let shelfTwoArr = []
+    let shelfThreeArr = []
+
+    let totalFridgeContents = ""
+
+    this.props.fridge.map((food) => {
+      if (food.shelf === 1){
+        shelfOneArr.push(food)
+       }
+       if (food.shelf === 2){
+       shelfTwoArr.push(food)
+       }
+       if (food.shelf === 3){
+       shelfThreeArr.push(food)
+      }
     })
+
+    // for each food item in shelf one
+      // create a div
+
+    // #########################################################################################################################
+    // return this.props.fridge.map((food) => {
+    //   if (food.shelf === 1){
+    //   return (
+    //     <div className='shelf1'><li key={food.id}>{food.item} {food.owner} {food.expDate} {food.shelf}</li></div>)
+    //   }
+    //   if (food.shelf === 2){
+    //   return (<div className='shelf2'><li key={food.id}>{food.item} {food.owner} {food.expDate} {food.shelf}</li></div>)
+    //   }
+    //   if (food.shelf === 3){
+    //   return (<div className='shelf3'><li key={food.id}>{food.item} {food.owner} {food.expDate} {food.shelf}</li></div>)
+    // #########################################################################################################################
+
+    // return (
+    //   // <li key={food.id}>{food.item} {food.owner} {food.expDate} {food.shelf}</li>
+    //     // <div key={food.id}>
+    //     //   <li>{food.item}</li>
+    //     //   <li>{food.expDate}</li>
+    //     //   <li>{food.owner}</li>
+    //     //   <li>{food.shelf}</li>
+    //     //   <hr/>
+    //     // </div>
+    // )
+    console.log("Shelf one: "+shelfOneArr)
+    console.log("Shelf two: "+shelfTwoArr)
+    console.log("Shelf three: "+shelfThreeArr)
   }
   render () {
     return (
@@ -31,5 +67,6 @@ const mapStateToProps = (state) => {
     fridge: state.fridge
   }
 }
+
 
 export default connect(mapStateToProps)(InsideFridge)
